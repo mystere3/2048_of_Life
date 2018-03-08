d6a4a098a228c783a6f9ea7808a6710896b49db7
mystere3 <mystere3@msn.com>
2018-03-07 23:00:20 -0500

    Initial Commit

---
+++ 2048_of_Life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048_of_Life
+//
+//  Created by Silissa Kenney on 3/7/18.
+//  Copyright © 2018 Eric Lehmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
