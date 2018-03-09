1f170f42931cd43d78dd909cdc0d2299aa48949a
mystere3 <mystere3@msn.com>
2018-03-09 01:06:06 -0500

    Ep4 11:40. stuck on Timer Interval

--- 2048_of_Life/Models/GameModel.swift
+++ 2048_of_Life/Models/GameModel.swift
@@ -6,4 +6,92 @@
 //  Copyright © 2018 Eric Lehmann. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol GameModelProtocol : class {
+    func scoreChange(score: Int)
+    func moveOneTile(from: (Int, Int), to: (Int, Int), value: Int)
+    func moveTwoTile(from: ((Int, Int), (Int, Int)), to: (Int, Int), value: Int)
+    func insertTile(location: (Int, Int), value: Int)
+}
+
+class GameModel: NSObject {
+    let dimension: Int
+    let threshold: Int
+    
+    var score: Int = 0 {
+        didSet {
+            delegate.scoreChanged(score)
+        }
+    }
+    var gameboard: SquareGameboard<TileObject>
+    
+    let delegate: GameModelProtocol
+    
+    var queue: [MoveCommand]
+    var timer: Timer
+    
+    let maxCommands = 100
+    let queueDelay = 0.3
+    
+    init(dimension d: Int, threshold t: Int, delegate: GameModelProtocol) {
+        dimension = d
+        threshold = t
+        self.delegate = delegate
+        queue = [MoveCommand]()
+        timer = Timer()
+        gameboard = SquareGameBoard(dimension: d, initialValue: .Empty)
+        super.init()
+    }
+    
+    func reset() {
+        score = 0
+        gameboard.setAll(.Empty)
+        queue.removeAll(keepCapacity: true)
+        timer.invalidate()
+    }
+    
+    func queueMove(direction: MoveDirection, completion: (Bool) -> ()){
+        if queue.count > maxCommands {
+            return
+        }
+        
+        let command = MoveCommand(d: direction, c: completion)
+        queue.append(command)
+        if(!timer.valid) {
+            timerFired(timer)
+        }
+    }
+    
+    func timerFired(timer: Timer) {
+        if queue.count == 0 {
+            return
+        }
+        
+        
+        var changed = false
+        while queue.count > 0 {
+            let command = queue[0]
+            queue.removeAtIndex(0)
+            changed = preformMove(command.direction)
+            command.completion(changed)
+            if changed {
+                break
+            }
+        }
+        if changed {
+            self.timer = Timer.scheduledTimer(withTimeInterval: <#T##TimeInterval#>, repeats: <#T##Bool#>, block: <#T##(Timer) -> Void#>)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
